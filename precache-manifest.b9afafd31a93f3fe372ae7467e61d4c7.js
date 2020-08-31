0e0f628b61754e395010a1ac7e4b4ab0d8f9a35b
hieunugent <hieunugent@gmail.com>
2020-08-30 18:30:50 -0700

    Updates

---
+++ precache-manifest.b9afafd31a93f3fe372ae7467e61d4c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d87ee5fc8fbf8abe45d0d73d3d23749",
+    "url": "/BunRieuTanDinh/index.html"
+  },
+  {
+    "revision": "bf9283cadf5d677ece0e",
+    "url": "/BunRieuTanDinh/static/css/main.df78a8c9.chunk.css"
+  },
+  {
+    "revision": "4f01e390771252c31bbc",
+    "url": "/BunRieuTanDinh/static/js/2.f50162b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BunRieuTanDinh/static/js/2.f50162b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9283cadf5d677ece0e",
+    "url": "/BunRieuTanDinh/static/js/main.67fe61ed.chunk.js"
+  },
+  {
+    "revision": "1ef85ca3a8ea66a6ecc0",
+    "url": "/BunRieuTanDinh/static/js/runtime-main.99cfeb34.js"
+  },
+  {
+    "revision": "190869acb50e09956cc06c06ab69544d",
+    "url": "/BunRieuTanDinh/static/media/logo.190869ac.jpg"
+  },
+  {
+    "revision": "cb9f1e15a8b4632da7269571f439652a",
+    "url": "/BunRieuTanDinh/static/media/page1.cb9f1e15.jpg"
+  },
+  {
+    "revision": "4b0cbf8fe5589b01b4f90fe9b8a96e45",
+    "url": "/BunRieuTanDinh/static/media/page2.4b0cbf8f.jpg"
+  },
+  {
+    "revision": "a441a1bcec42b743694e807fa4d3d918",
+    "url": "/BunRieuTanDinh/static/media/page3.a441a1bc.jpg"
+  },
+  {
+    "revision": "bccd15b5eddbd1263fce2389e2819495",
+    "url": "/BunRieuTanDinh/static/media/page4.bccd15b5.jpg"
+  },
+  {
+    "revision": "971c5e299c7c880508eb55bd50ef83fb",
+    "url": "/BunRieuTanDinh/static/media/page5.971c5e29.jpg"
+  }
+]);
